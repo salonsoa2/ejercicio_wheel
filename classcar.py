170769f7dbba0697cb9528c676a2cbd0ea99dbb6
salonsoa2 <salonsoa2@alumnos.nebrija.es>
2021-12-02 11:18:37 +0100

    ejercicio9

---
+++ classcar.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, marca, modelo, combustible, cilindrada):
+        self.mar = marca
+        self.mod = modelo
+        self.comb = combustible
+        self.cili = cilindrada
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import Wheel as wh
+import classcar as cc
 
-rueda = wh.Wheel(10, 3, 5)
+mercedes_r350 = cc.Car("mercedes", "r350", "gasolina", "3500")
+print("marca coche:", mercedes_r350.mar)
+print("modelo del coche:", mercedes_r350.mod)
+print("Tipo de combustible que lleva:", mercedes_r350.comb)
+print("cilindrada del coche:", mercedes_r350.cili)
+
+rueda = wh.Wheel(100, 3, 5)
 print("diametro rueda:", rueda.mm)
 print("rodadura:", rueda.rod)
 print("ancho:", rueda.pulgadas)
